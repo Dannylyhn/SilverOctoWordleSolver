4384101def849d10e27dad6c5765d6e153bdd760
Danny <dannylyhn@gmail.com>
2023-08-08 13:54:11 +0200

    Intial Setup

---
+++ WordleSolverAPI/WordleSolverAPI/Controllers/WordleSolverController.cs
@@ -0,0 +1,23 @@
+﻿namespace WordleSolverAPI.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/wordleSolver")]
+    public class WordleSolverController
+    {
+        private readonly ILogger<WordleSolverController> _logger;
+
+        public WordleSolverController(ILogger<WordleSolverController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Solve")]
+        public string Solve()
+        {
+            return "Hello";
+        }
+        
+    }
+}
