fede7206bcffb7d8bd33e6dd334f4483b5d3f64b
Danny <dannylyhn@gmail.com>
2023-08-08 15:02:19 +0200

    Read txt-file of words into a list

--- WordleSolverAPI/WordleSolverService/API/WordleSolver.cs
+++ WordleSolverAPI/WordleSolverService/API/WordleSolver.cs
@@ -4,7 +4,34 @@
     {
         public string SolveWordle()
         {
+            this.readTextFile();
             return "It is solved";
+        }
+
+        public List<string> readTextFile()
+        {
+            var listOfWords = new List<string>();
+
+            try
+            {
+                // Open the text file using a stream reader.
+                using (var sr = new StreamReader("listOf5LetterWords.txt"))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string word = sr.ReadLine();
+                        listOfWords.Add(word);
+                        
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            return listOfWords;
         }
     }
 }
